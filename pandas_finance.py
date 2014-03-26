3f3818e4a21ffc4e1b8d4426093fc093396b5a5b
David McKee <dragon@scraperwiki.com>
2014-03-26 15:53:13 +0000

    Use pandas native saving by forcing date to not be index, and be string

--- pandas_finance.py
+++ pandas_finance.py
@@ -1,56 +1,34 @@
 #!/usr/bin/env python
 import datetime
-
-import scraperwiki
-import numpy
+import sqlite3
 import pandas.io.data as web
+import pandas.io.sql as sql
 
 
-def get_stock(stock, start, end, service):
+def get_stock(stock, start, end):
     """
-    Return data frame of finance data for stock.
+    Return data frame of Yahoo Finance data for stock.
 
-    Takes start and end datetimes, and service name of 'google' or 'yahoo'.
+    Takes start and end datetimes.
     """
-    return web.DataReader(stock, service, start, end)
+    return web.DataReader(stock, 'yahoo', start, end)
 
-
-def parse_finance_frame(stock, start, end, service='google'):
-    """
-    Return rows of dicts from a finance data frame for scraperwiki.sqlite.
-
-    service can also be 'yahoo', start and end are datetimes.
-    """
-    frame = get_stock(stock, start, end, service)
-    rows = []
-    for idx in range(len(frame)):
-        current_row_as_dict = frame.ix[idx].to_dict()
-        # have to convert dates because these are Pandas timestamps and
-        # dumptruck doesn't support them
-        current_row_as_dict['Date'] = frame.index[idx].to_datetime()
-        current_row_as_dict['Stock'] = stock
-        # horrible hack because data values are numpy.float64 and dumptruck
-        # doesn't support them
-        for key in current_row_as_dict:
-            if isinstance(current_row_as_dict[key], numpy.float64):
-                current_row_as_dict[key] = float(current_row_as_dict[key])
-        rows.append(current_row_as_dict)
-    return rows
-
+def scrape_stock(stock, start, end):
+    sqlite_db.execute("drop table if exists {};".format(stock))
+    frame = (get_stock(stock, start, end))
+    # make Date not an index so it appears in table
+    frame = frame.reset_index()
+    # force Date datetime to string
+    frame[['Date']] = frame[['Date']].applymap(lambda x: x.isoformat())
+    sql.write_frame(frame, stock, sqlite_db)
 
 def main():
-    """
-    Dump stock data into scraperwiki.sqlite using pandas.io.data.
-    """
-    # arbitrary start chosen
+    global sqlite_db
+    sqlite_db = sqlite3.connect("scraperwiki.sqlite")
     start = datetime.datetime(2014, 3, 1)
     end = datetime.datetime.today()
-
-    stock_list = ['TWTR', 'FB']
-    rows = []
-    for stock in stock_list:
-        rows.extend(parse_finance_frame(stock, start, end))
-    scraperwiki.sqlite.save(data=rows, unique_keys=['Stock', 'Date'])
-
+    for ticker in ['TWTR', 'FB']:
+        scrape_stock(ticker, start, end)
+    
 if __name__ == '__main__':
     main()
