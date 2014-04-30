ac9e9b468e33ef4ee6233923db4abf443640789d
Steven Maude <steve@scraperwiki.com>
2014-04-30 11:00:06 +0100

    Add initial test.

---
+++ tests.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import datetime
+import unittest
+
+import mock
+
+from nose.tools import assert_equal, assert_is_instance
+
+from pandas_finance import get_stock
+
+class TestGetStock(unittest.TestCase):
+    @mock.patch('pandas_finance.web.DataReader')
+    def test_called_correctly(self, mock_datareader):
+        mock_datareader()
+        start = datetime.datetime(1999, 4, 3, 0, 0)
+        end = datetime.datetime(2005, 2, 5, 0, 0)
+        get_stock('AAPL', start, end)
+        mock_datareader.assert_called_with('AAPL', 'yahoo', start, end)
