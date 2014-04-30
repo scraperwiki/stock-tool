f5f850e53a889a5afe483ae2ca07e147d4a94c08
Steven Maude <steve@scraperwiki.com>
2014-04-30 11:29:35 +0100

    Add test for parsing tickers.

--- tests.py
+++ tests.py
@@ -6,8 +6,7 @@ import unittest
 import mock
 
 from nose.tools import assert_equal, assert_is_instance
-
-from pandas_finance import get_stock
+from pandas_finance import get_stock, get_required_tickers
 
 class PandasFinanceTestCase(unittest.TestCase):
     @mock.patch('pandas_finance.web.DataReader')
@@ -17,3 +16,10 @@ class PandasFinanceTestCase(unittest.TestCase):
         end = datetime.datetime(2005, 2, 5, 0, 0)
         get_stock('AAPL', start, end)
         mock_datareader.assert_called_with('AAPL', 'yahoo', start, end)
+
+    def test_get_required_tickers_parses_tickers_with_newline(self):
+        m = mock.mock_open(read_data='TWTR,FB,AAPL,MSFT\n')
+        textfile = None  # only used to provide valid argument
+        with mock.patch('pandas_finance.open', m, create=True):
+            result = get_required_tickers(textfile)
+        assert_equal('TWTR,FB,AAPL,MSFT', result)
