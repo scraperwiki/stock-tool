d1a85b9ae421ddb4a4c9539cdca50ec9a7c6de72
Steven Maude <steve@scraperwiki.com>
2014-04-30 16:05:29 +0100

    Import pandas_finance, not functions from it.
    Think this appears to make mocking "with open..." simpler.

--- tests.py
+++ tests.py
@@ -4,9 +4,9 @@ import datetime
 import unittest
 
 import mock
+import pandas_finance
 
 from nose.tools import assert_equal
-from pandas_finance import get_stock, get_required_tickers
 
 
 class GetStockTestCase(unittest.TestCase):
@@ -14,12 +14,12 @@ class GetStockTestCase(unittest.TestCase):
     def test_get_stock_called_correctly(self, mock_datareader):
         start = datetime.datetime(1999, 4, 3, 0, 0)
         end = datetime.datetime(2005, 2, 5, 0, 0)
-        get_stock('AAPL', start, end)
+        pandas_finance.get_stock('AAPL', start, end)
         mock_datareader.assert_called_with('AAPL', 'yahoo', start, end)
 
     def test_get_required_tickers_parses_tickers_with_newline(self):
         m = mock.mock_open(read_data='TWTR,FB,AAPL,MSFT\n')
         textfile = None  # only used to provide valid argument
         with mock.patch('pandas_finance.open', m, create=True):
-            result = get_required_tickers(textfile)
+            result = pandas_finance.get_required_tickers(textfile)
         assert_equal('TWTR,FB,AAPL,MSFT', result)
